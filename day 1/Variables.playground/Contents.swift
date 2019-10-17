16200c31fc25817ddf9247ee7e8ad5bbea5622e6
Daniel Ferecatu <fere@minglebit.com>
2019-10-17 22:57:26 +0300

    Completed "Variables" step

---
+++ day 1/Variables.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var string = "Hello, Xcode!"
+
+var con = str + string
+
+var multiline =
+"""
+\(str)
+\(string)
+"""
+
+str = "Goodbye"
+
+let a=0, b=0, c=0
+let aa:Int=4, bb:Int=3, cc:Int=2
+a
+b
+c
+
+aa
+bb
+cc
+
+let aaa, bbb, ccc: Double
+
+let stone = 0000.0
+
+//stone = 46
+
+var variabilă = 464646
+
+var `var` = "var"; print(`var`, terminator: "")
