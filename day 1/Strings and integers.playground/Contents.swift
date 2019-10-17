550e444525cf283d1281ffd1f30a99578d392d05
Daniel Ferecatu <fere@minglebit.com>
2019-10-17 23:23:03 +0300

    Completed "Strings and integers" step

---
+++ day 1/Strings and integers.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Cocoa
+
+var str = "Hello, playground"
+var age = 38
+var integer_with_underscore_separator = 1_000_000
+
+// pretty considerable values
+let imfimum = Int.min
+let supremum = Int.max
+let manual = 9_223_372_036_854_775_807
+//let doNotTryEVER = 9_223_372_036_854_775_808
+let bigManual: UInt = 18_446_744_073_709_551_615
+
+var mostInfimum = UInt8.min
+var mustInfimum = Int64.min
+var inf = CInt.init(300)
+
+// see the difference?
+let supremus = UINT64_MAX
+let supremuS = UInt64.max
+
