85e8164372ad431e2d004f39cf3b5ed6e1aa1093
Daniel Ferecatu <fere@minglebit.com>
2019-12-15 14:50:58 +0200

    Completed "Doubles and booleans" step

---
+++ day 1/Doubles and booleans.playground/Contents.swift
@@ -0,0 +1,13 @@
+var pi = 3.141
+
+var awesome = true
+
+var booleanChecking = 1
+if booleanChecking == 1 {
+    print("bool is 1")
+}
+
+// Option+Click variable name to display type info
+var float = Float(3.141)
+var double = Double(3.141)
+var doublE = 3.141
