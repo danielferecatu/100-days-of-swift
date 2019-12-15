a4fc7551d23e90416cee393fa348b7880a9443c7
Daniel Ferecatu <fere@minglebit.com>
2019-12-15 18:06:41 +0200

    Completed "Simple types: Summary" step

---
+++ day 1/Simple types- Summary.playground/Contents.swift
@@ -0,0 +1,35 @@
+// ONE
+
+var str = "Hello, playground"
+let taylor = "swift"
+
+
+// TWO
+
+let str1 = """
+This goes
+over multiple
+lines
+"""
+
+
+// THREE
+
+let age = 38
+let pi = 3.141
+let there = "be light"
+let isLight = true
+
+
+// FOUR
+
+let score = 99.999
+let result = "Almost there: \(score)"
+
+
+// FIVE
+
+let album: String = "Reputation"
+let year: CShort = 1989
+let height: Float = 1.88
+let isShiny: Bool = true
