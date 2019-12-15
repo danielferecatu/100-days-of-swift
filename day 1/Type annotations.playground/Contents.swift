02f551529c4a6998a2ee7b299b244fa8fc9a7712
Daniel Ferecatu <fere@minglebit.com>
2019-12-15 16:39:32 +0200

    Completed "Type annotations" step

---
+++ day 1/Type annotations.playground/Contents.swift
@@ -0,0 +1,10 @@
+let str = "Hello, playground!"
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+let compoundType: (Int, Int) = (0, 0)
+
+let double: Double = 100_000
