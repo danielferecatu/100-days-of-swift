fbf9e6a235f34d9f18e7ecbd259fbe4836c1e6f7
Daniel Ferecatu <fere@minglebit.com>
2019-12-17 15:41:08 +0200

    Completed "Tuples" step

---
+++ day-2/Tuples.playground/Contents.swift
@@ -0,0 +1,10 @@
+var name = (first: "Taylor", last: "Swift")
+
+name.0
+name.last
+
+name = (first: "Jony", last: "Ive")
+name = ("Craig", "Federighi")
+
+// Value of tuple type '(first: String, last: String)' has no member '2'
+//name.2
