20a61607791f412ab7a7eef2d974f61b0659cbd7
Daniel Ferecatu <fere@minglebit.com>
2020-01-24 12:26:22 +0200

    Completed "Compound assignment operators" step

---
+++ day 3/Compound assignment operators.playground/Contents.swift
@@ -0,0 +1,15 @@
+var score = 95
+score -= 5
+
+var quote = "The rain in Spain falls mainly on the "
+quote += "Spaniards"
+
+var `true` = true
+!`true`
+
+var positive = 1
+-positive
+
+var negative = -1
+-negative
+
