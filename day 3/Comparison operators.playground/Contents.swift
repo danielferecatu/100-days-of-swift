f3536f4371babc20b49742c71a8951ed9f3430ea
Daniel Ferecatu <fere@minglebit.com>
2020-01-24 12:27:03 +0200

    Completed "Comparison operators" step

---
+++ day 3/Comparison operators.playground/Contents.swift
@@ -0,0 +1,23 @@
+let firstScore = 6
+let secondScore = 4
+
+firstScore == secondScore
+firstScore != secondScore
+
+firstScore < secondScore
+firstScore >= secondScore
+
+"Taylor" <= "Swift"
+
+
+1 == 1  // true  beacuse 1 is equal to 1
+2 != 1  // true  because 2 is not equal to 1
+2 > 1   // true  because 1 is less than 2
+1 < 2   // true  because 2 is greater than 1
+1 >= 1  // true  because 1 is greater than or equal to 1
+2 <= 1  // false because 2 is not less than or equal to 1
+
+
+(1, "zebra") < (2, "apple")  // true because 1 is less than 2; "zebra" and "apple" are not compared
+(3, "apple") < (3, "bird")   // true because 3 is equal to 3;  "apple" is less than "bird"
+(4, "dog") == (4, "dog")     // true because 4 is equal to 4;  "dog" is equal to "dog"
